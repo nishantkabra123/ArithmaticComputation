b6e83120da952fe8aaf67ee283a14e67f1b4fcd4
nishantkabra123 <nishantkabra123@gmail.com>
2020-11-23 19:59:20 +0530

    Refractored to show descending

--- ArithmaticSorting.sh
+++ ArithmaticSorting.sh
@@ -2,40 +2,40 @@
 read -p "Enter a : " a
 read -p "Enter b : " b
 read -p "Enter c : " c
-result2=$(($a+($b*$c)))
-result3=$((($a*$b)+$c))
-result4=$(($c+($a/$b)))
-result5=$((($a%$b)+$c))
+result1=$(($a+($b*$c)))
+result2=$((($a*$b)+$c))
+result3=$(($c+($a/$b)))
+result4=$((($a%$b)+$c))
 
 declare -A results
 
-for i in {2..5}
+for i in 1 2 3 4  
 do
 	results[$i]=`echo "$((result$i))"`
-    compute[(($i-2))]=${results[$i]}
+    compute[$i]=${results[$i]}
 done
 
 function sortArray(){
-	array=("$@")
-	for i in ${!array[@]}
+array=("$@")
+for i in ${!array[@]}
+do
+	for j in ${!array[@]}
 	do
-		for j in ${!array[@]}
-		do
-			if [ ${array[$i]} -lt ${array[$j]} ]
-			then
+		if [ ${array[$i]} -lt ${array[$j]} ]
+		then
 				temp=${array[$i]}
 				array[$i]=${array[$j]}
 				array[$j]=$temp
-			fi
-		done
+		fi
 	done
+done
 	echo "Array in ascending order is : " ${array[@]}
 }
 
-echo "first result is $result2"
-echo "second result 0is $result3"
-echo "third result is $result4"
-echo "fourth result is $result5"
+echo "first result is $result1"
+echo "second result is $result2"
+echo "third result is $result3"
+echo "fourth result is $result4"
 
 echo "results in dictionary are : "${results[@]}
 echo "results in array are : "${compute[@]}
