ae399adfdfbc9dd74bfcf44afdab4fec8583e7dc
nishantkabra123 <nishantkabra123@gmail.com>
2020-11-23 18:18:50 +0530

    added a%b+c

--- ArithmaticSorting.sh
+++ ArithmaticSorting.sh
@@ -7,10 +7,12 @@ read -p "enter c" c
 result1=$(($a+($b*$c)))
 result2=$((($a*$b)+$c))
 result3=$(($c+($a/$b)))
+result4=$((($a%$b)+$c))
 
 echo "first result is $result1"
 echo "second result is $result2"
 echo "third result is $result3"
+echo "third result is $result4"
 
 
 
